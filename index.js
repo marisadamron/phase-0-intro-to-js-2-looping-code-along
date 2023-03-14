bab9942dc4474108e21031c9f89286e26e2e8c19
Marisa Damron <mmarisadamronn@icloud.com>
2023-03-14 07:10:57 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+    let message=[]
+    for (let i = 0; i < name.length; i ++) {
+        message.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)}
+        return message;
+}
+
+
+
+function countDown(number){
+    while (number >= 0){
+        console.log(number);
+        number --;   
+     }}
+
+let newCount = [4]
+function newCountDown() { while (newCount > -1) {
+    console.log(newCount--)
+}}
